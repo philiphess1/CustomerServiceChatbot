6b3d8e5a1253a9e5dbe0d1ccad9d0b8e05143506
nfrische <nfrische@iu.edu>
2023-11-27 20:37:50 -0500

    sidebar

--- static/js/admin.js
+++ static/js/admin.js
@@ -164,3 +164,15 @@ function updateFileSizeAndCount() {
         fileInput.value = '';
     }
 }
+
+// sidebar JS
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
