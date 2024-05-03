d6a88b2b244a0bcc08dc4605e64e3fd4c9dd4885
phjhess <phjhess@iu.edu>
2024-05-03 13:17:57 -0400

    Finalized integrations page, changed background colors of pages, and added icons to copy/paste code

--- static/js/integrations.js
+++ static/js/integrations.js
@@ -54,7 +54,49 @@ for (var i = 0; i < btns.length; i++) {
         btns[i].style.backgroundColor = '#444';
     }
 }
-function copyCode() {
-    var code = document.getElementById('chatbot-code').innerText;
-    navigator.clipboard.writeText(code);
+function copyCode(element) {
+    var textToCopy = element.previousElementSibling.textContent; // Get the text from the preceding pre tag
+    navigator.clipboard.writeText(textToCopy).then(function() {
+        var tooltip = element.nextElementSibling;
+        tooltip.textContent = "Copied!";
+        tooltip.style.display = "block";
+        tooltip.style.backgroundColor = "#88bf46";
+        tooltip.style.color = "white";
+        setTimeout(function() {
+            tooltip.style.display = "none";
+            tooltip.textContent = "Copy to Clipboard";
+            tooltip.style.backgroundColor = "";
+            tooltip.style.color = "";
+        }, 2000); // Hide the tooltip after 2 seconds
+    }, function(err) {
+        console.error('Could not copy text: ', err);
+    });
+}
+
+function copyIframeCode(element) {
+    var textToCopy = element.previousElementSibling.textContent; // Get the text from the preceding pre tag
+    navigator.clipboard.writeText(textToCopy).then(function() {
+        var tooltip = element.nextElementSibling;
+        tooltip.textContent = "Copied!";
+        tooltip.style.display = "block";
+        tooltip.style.backgroundColor = "#88bf46";
+        tooltip.style.color = "white";
+        setTimeout(function() {
+            tooltip.style.display = "none";
+            tooltip.textContent = "Copy to Clipboard";
+            tooltip.style.backgroundColor = "";
+            tooltip.style.color = "";
+        }, 2000); // Hide the tooltip after 2 seconds
+    }, function(err) {
+        console.error('Could not copy text: ', err);
+    });
+}
+function showTooltip(element) {
+    var tooltip = element.nextElementSibling;
+    tooltip.style.display = "block";
+}
+
+function hideTooltip(element) {
+    var tooltip = element.nextElementSibling;
+    tooltip.style.display = "none";
 }
