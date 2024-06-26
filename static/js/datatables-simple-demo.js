aed4da76b3524c5fddda3ab9b751c47e182ed80c
nfrische <nfrische@iu.edu>
2024-06-26 00:40:48 -0400

    conditionally see questions on table

--- static/js/datatables-simple-demo.js
+++ static/js/datatables-simple-demo.js
@@ -1,18 +1,1 @@
-window.addEventListener('DOMContentLoaded', event => {
-    // Simple-DataTables
-    // https://github.com/fiduswriter/Simple-DataTables/wiki
 
-    const datatablesSimple = document.getElementById('datatablesSimple');
-    if (datatablesSimple) {
-        let rows = datatablesSimple.querySelectorAll('tbody tr');
-        if (rows.length > 10) {
-            for (let i = 10; i < rows.length; i++) {
-                rows[i].parentNode.removeChild(rows[i]);
-            }
-        }
-
-        new simpleDatatables.DataTable(datatablesSimple, {
-            paging: false // Ensure paging is disabled
-        });
-    }
-});
