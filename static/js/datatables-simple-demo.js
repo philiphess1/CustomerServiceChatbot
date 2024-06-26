577e5b16660c148cc27fa565b748b7f3aa7c9d3a
phjhess <phjhess@iu.edu>
2024-06-25 21:35:56 -0500

    Figured out how to limit rows in table or disable pagination depending upon certain criteria

--- static/js/datatables-simple-demo.js
+++ static/js/datatables-simple-demo.js
@@ -4,6 +4,15 @@ window.addEventListener('DOMContentLoaded', event => {
 
     const datatablesSimple = document.getElementById('datatablesSimple');
     if (datatablesSimple) {
-        new simpleDatatables.DataTable(datatablesSimple);
+        let rows = datatablesSimple.querySelectorAll('tbody tr');
+        if (rows.length > 10) {
+            for (let i = 10; i < rows.length; i++) {
+                rows[i].parentNode.removeChild(rows[i]);
+            }
+        }
+
+        new simpleDatatables.DataTable(datatablesSimple, {
+            paging: false // Ensure paging is disabled
+        });
     }
 });
